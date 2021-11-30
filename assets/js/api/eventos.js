ca94fcddb9fa6b54def808aa15fb45b225e1f867
YosmanProgramer <Yosman.higuita028@gmail.com>
2021-11-30 16:40:18 -0500

    fix js file

--- assets/js/api/eventos.js
+++ assets/js/api/eventos.js
@@ -9,18 +9,22 @@ const images = document.querySelectorAll('.accordion--images');
 // EVENTS HANDLERS
 
 // PARENT ELEMENT
-filters.addEventListener('click', function(e) {
-    const child = e.target.closest(".accordion");
-    const id = child.getAttribute('data-tab');
-    if (panels[id].style.maxHeight) {
-        panels[id].style.maxHeight = null;
-        panels[id].style.padding = "0px";
-        panels[id].style.overflowY = "hidden";
-        images[id].style.transform = "rotate(0deg)";
-    } else {
-        panels[id].style.maxHeight = 193 + "px";
-        panels[id].style.padding = "16px 24px 8px 24px";
-        panels[id].style.overflowY = "scroll";
-        images[id].style.transform = "rotate(90deg)";
+filters.addEventListener('click', function (e) {
+    try {
+        const child = e.target.closest(".accordion");
+        const id = child.getAttribute('data-tab');
+        if (panels[id].style.maxHeight) {
+            panels[id].style.maxHeight = null;
+            panels[id].style.padding = "0px";
+            panels[id].style.overflowY = "hidden";
+            images[id].style.transform = "rotate(0deg)";
+        } else {
+            panels[id].style.maxHeight = 193 + "px";
+            panels[id].style.padding = "16px 24px 8px 24px";
+            panels[id].style.overflowY = "scroll";
+            images[id].style.transform = "rotate(90deg)";
+        }
+    } catch {
+        return;
     }
 })
