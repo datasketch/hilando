648b25132439af3470eb225ee40220b0dcccd5c3
David Daza <03dazal@gmail.com>
2022-01-20 09:29:30 -0500

    remove unnecessary config

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,7 @@ module.exports = {
     extend: {
       fontFamily: {},
       backgroundImage: {
-        'banner-home-sm': 'url(/images/home/banner-home.png)',
-        'banner-home-lg': 'url(/images/home/banner-home@2x.png)',
+        'banner-home': 'url(/images/home/banner-home.png)',
         'banner-territorios': 'url(/images/territorios/banner-territorios.svg)',
         'banner-noticias': 'url(/images/noticias/banner-noticias.svg)',
         'banner-eventos': 'url(/images/eventos/banner-eventos.svg)',
