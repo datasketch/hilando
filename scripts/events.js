d73de6f61e2b01bfe171ff9ce7d81102d5ae5787
LuisCalderonGit <luis2fcalderon@gmail.com>
2022-05-23 11:36:55 -0500

    Added event.js to modify json events

--- scripts/events.js
+++ scripts/events.js
@@ -1,11 +1,27 @@
+const fs = require('fs/promises');
 const data = require('../data/eventos.json');
+const path = require('path');
 
-data.map((item) => {
+const dataEvents=data.map((item) => {
   return {
     ...item,
-    // foto: ['url1', 'url2'],
-    // foto: [],
-    // thumbnail: 'url1' || null
+    foto: (JSON.parse(item.foto))?.map((fot) => fot.url) || [],
+    thumbnail: (JSON.parse(item.foto))?.map((fot) => fot.url)[0] || null,
   };
 });
 
+const getFilePath = (filename) => {
+  return path.join(__dirname, '..', 'data', filename);
+};
+
+async function run() {
+  fs.writeFile(
+      getFilePath('eventos.json'),
+      JSON.stringify(dataEvents),
+  );
+}
+
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
