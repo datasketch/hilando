0acafa6f554ad59be64a175805fe7060cd4f84ee
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2022-05-31 13:56:32 -0500

    update community layout

--- scripts/reports.js
+++ scripts/reports.js
@@ -8,6 +8,8 @@ const slugMunicipalityNames = trrs.map((trr) => trr.municipio.toLowerCase().repl
 
 const slugCommunityNames = communityData.map((comm) => comm.nombre_comunidad.toLowerCase().replace(/\s/g, '-').normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
 
+const slugs = [...slugMunicipalityNames, ...slugCommunityNames];
+
 const domain = 'https://www.hilandocomunidades.com';
 
 (async () => {
@@ -20,46 +22,16 @@ const domain = 'https://www.hilandocomunidades.com';
       },
     });
     const page = await browser.newPage();
-    for (let index = 0; index < slugMunicipalityNames.length; index++) {
-      console.log(`Generando PDF (${slugMunicipalityNames[index]})`);
-      await page.goto(`${domain}/reportes/${slugMunicipalityNames[index]}/`, {
+    for (let index = 0; index < slugs.length; index++) {
+      console.log(`Generando PDF (${slugs[index]})`);
+      await page.goto(`${domain}/reportes/${slugs[index]}/`, {
         waitUntil: 'networkidle0',
       });
       const pdf = await page.pdf({
         format: 'letter',
         printBackground: true,
       });
-      await fs.writeFile(path.join(__dirname, '..', 'static', 'reportes', `${slugMunicipalityNames[index]}.pdf`), pdf);
-      console.log('PDF generado');
-      console.log('~~~');
-    }
-    await browser.close();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-})();
-
-(async () => {
-  try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      defaultViewport: {
-        width: 1366,
-        height: 768,
-      },
-    });
-    const page = await browser.newPage();
-    for (let index = 0; index < slugCommunityNames.length; index++) {
-      console.log(`Generando PDF (${slugCommunityNames[index]})`);
-      await page.goto(`${domain}/reportes/${slugCommunityNames[index]}/`, {
-        waitUntil: 'networkidle0',
-      });
-      const pdf = await page.pdf({
-        format: 'letter',
-        printBackground: true,
-      });
-      await fs.writeFile(path.join(__dirname, '..', 'static', 'reportes', `${slugCommunityNames[index]}.pdf`), pdf);
+      await fs.writeFile(path.join(__dirname, '..', 'static', 'reportes', `${slugs[index]}.pdf`), pdf);
       console.log('PDF generado');
       console.log('~~~');
     }
