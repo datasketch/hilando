eff5be6e97bbfa17de2183b79451eff8c4c6e624
yoshi <yosman@datasketch.co>
2024-01-29 10:59:24 -0500

    add scroll to sections

--- assets/js/resilientes.js
+++ assets/js/resilientes.js
@@ -1,7 +1,20 @@
 /* eslint-disable no-unused-vars */
 import {swiperResilientesEntities, swiperResilientesHistories, swiperResilientesRead, swiperResilientesViews} from './lib/slider';
 
+const navigation = document.getElementById('navigation');
 const sliderResilientesHistories = swiperResilientesHistories('.swiper-resilientes-histories');
 const sliderResilientesViews = swiperResilientesViews('.swiper-resilientes-views');
 const sliderResilientesRead = swiperResilientesRead('.swiper-resilientes-read');
 const sliderResilientesEntities = swiperResilientesEntities('.swiper-resilientes-entities');
+
+navigation.addEventListener('click', (e) => {
+  e.preventDefault();
+  const link = e.target.closest('a');
+  const id = link.getAttribute('href');
+  const coords = document.querySelector(id).getBoundingClientRect();
+  window.scrollTo({
+    left: (coords.left + window.scrollX) - 130,
+    top: (coords.top + window.scrollY) - 130,
+    behavior: 'smooth',
+  });
+});
