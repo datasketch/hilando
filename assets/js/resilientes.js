3d0b7ca9c8e5e89fd0fc33760c40d4e3805baf53
yoshi <yosman@datasketch.co>
2024-01-29 10:24:31 -0500

    Implementar landing Resilientes

---
+++ assets/js/resilientes.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import {swiperResilientesEntities, swiperResilientesHistories, swiperResilientesRead, swiperResilientesViews} from './lib/slider';
+
+const sliderResilientesHistories = swiperResilientesHistories('.swiper-resilientes-histories');
+const sliderResilientesViews = swiperResilientesViews('.swiper-resilientes-views');
+const sliderResilientesRead = swiperResilientesRead('.swiper-resilientes-read');
+const sliderResilientesEntities = swiperResilientesEntities('.swiper-resilientes-entities');
