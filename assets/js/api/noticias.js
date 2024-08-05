5c16840374c33899e30022e863ed353770a589be
yoshi <yosman@datasketch.co>
2024-08-05 09:11:06 -0500

    DSPRJT-6511: Implementar ajustes a página de noticias

--- assets/js/api/noticias.js
+++ assets/js/api/noticias.js
@@ -2,7 +2,7 @@ const filters = document.querySelector('.filters');
 const panels = document.querySelectorAll('.panel');
 const images = document.querySelectorAll('.accordion--images');
 
-filters.addEventListener('click', function (e) {
+filters.addEventListener('click', function(e) {
   const child = e.target.closest('.accordion');
   if (!child) return;
   const id = child.getAttribute('data-tab');
@@ -12,15 +12,15 @@ filters.addEventListener('click', function (e) {
     panels[id].style.overflowY = 'hidden';
     images[id].style.transform = 'rotate(0deg)';
     panels[id]
-      .querySelectorAll('input')
-      .forEach((input) => input.setAttribute('disabled', 'true'));
+        .querySelectorAll('input')
+        .forEach((input) => input.setAttribute('disabled', 'true'));
   } else {
     panels[id].style.maxHeight = 193 + 'px';
     panels[id].style.padding = '16px 24px 8px 24px';
     panels[id].style.overflowY = 'scroll';
     images[id].style.transform = 'rotate(90deg)';
     panels[id]
-      .querySelectorAll('input')
-      .forEach((input) => input.removeAttribute('disabled'));
+        .querySelectorAll('input')
+        .forEach((input) => input.removeAttribute('disabled'));
   }
 });
